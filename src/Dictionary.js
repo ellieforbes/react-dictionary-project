9fab6759be9e414f037bbd9e0472610d48e892aa
ellieforbes <139644804+ellieforbes@users.noreply.github.com>
2023-11-23 19:30:47 +0000

    Integrated API function

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
 
     let [keyword, setKeyword] = useState("");
+
+    function handleResponse(response) {
+        console.log(response.data);
+    }
     function search(event){
         event.preventDefault();
         alert(`Searching for ${keyword} definition`)
+
+        let apiKey = "b5a70e3dbaf3379o5576fffe161ca0t4";
+        let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+   
+        axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event){
