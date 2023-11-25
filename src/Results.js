d6f8718dd3ac285519e78e1b7cf73eff8ae69fcc
ellieforbes <139644804+ellieforbes@users.noreply.github.com>
2023-11-25 19:21:00 +0000

    Integrated phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
+    console.log(props.results);
     if (props.results) {
     return (
         <div className="Results">
             <h2>{props.results.word}</h2>
+            <Phonetics phonetics={props.results.phonetic}/>
             {props.results.meanings.map(function (meaning, index) {
                 return (
                 <div key={index}>
